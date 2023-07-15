046b0006ea649eba7437fdfcbb7d815452000718
svartse <killbillbeatrixx@gmail.com>
2023-07-15 13:28:31 +0300

    header

--- src/main.js
+++ src/main.js
@@ -2,3 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "font-awesome/css/font-awesome.min.css";
